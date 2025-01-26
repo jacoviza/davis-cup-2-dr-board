584a318dd70f031f85b897d64e9389a9d628962c
Jacobo Villarreal <jvillarreal@neural.com.do>
2025-01-26 19:11:34 -0400

    Store form fields

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -2,8 +2,23 @@
 "use server";
 import { neon } from "@neondatabase/serverless";
 
+export async function create(formData: FormData) {
+    // Connect to the Neon database
+    const sql = neon(`${process.env.DATABASE_URL}`);
+
+    const jsonObject: { [key: string]: any } = {};
+    formData.forEach((value, key) => {
+        jsonObject[key] = value;
+    });
+
+    console.log('jsonObject:', jsonObject);
+
+    await sql('UPDATE match_board SET form = $1::jsonb WHERE game = \'dom-bol-02-2025\'',
+        [JSON.stringify(jsonObject)]);
+}
+
 export async function getData() {
-    const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`...`;
-    return data;
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const data = await sql('SELECT form FROM match_board WHERE game = \'dom-bol-02-2025\'');
+    return data[0].form;
 }
--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,15 +1,67 @@
-import {neon} from "@neondatabase/serverless";
+'use client';
+import {useEffect, useState} from "react";
+import {create, getData} from "@/app/actions";
 
 export default function Board() {
 
-    async function create(formData: FormData) {
-        'use server';
-        // Connect to the Neon database
-        const sql = neon(`${process.env.DATABASE_URL}`);
-        const day1dom1 = formData.get('day1dom1');
-        // Insert the comment from the form into the Postgres database
-        await sql('UPDATE match_board SET form = jsonb_set(form, \'{day1dom1}\', $1::jsonb) WHERE game = \'dom-bol-02-2025\'', [JSON.stringify(day1dom1)]);
-    }
+    const [formData, setFormData] = useState({
+        day1dom1: '',
+        day1dom2: '',
+        day1bol1: '',
+        day1bol2: '',
+        day2dom1: '',
+        day2dom2: '',
+        day2dom3: '',
+        day2dom4: '',
+        day2bol1: '',
+        day2bol2: '',
+        day2bol3: '',
+        day2bol4: '',
+        day1result1: '',
+        day1result2: '',
+        day2result1: '',
+        day2result2: '',
+        day2result3: ''
+        // Add other form fields as needed
+    });
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await getData();
+            console.log(data);
+            setFormData({
+                day1dom1: data.day1dom1,
+                day1dom2: data.day1dom2,
+                day1bol1: data.day1bol1,
+                day1bol2: data.day1bol2,
+
+                day2dom1: data.day2dom1,
+                day2dom2: data.day2dom2,
+                day2dom3: data.day2dom3,
+                day2dom4: data.day2dom4,
+
+                day2bol1: data.day2bol1,
+                day2bol2: data.day2bol2,
+                day2bol3: data.day2bol3,
+                day2bol4: data.day2bol4,
+
+                day1result1: data.day1result1,
+                day1result2: data.day1result2,
+                day2result1: data.day2result1,
+                day2result2: data.day2result2,
+                day2result3: data.day2result3,
+
+                // Set other form fields as needed
+            });
+        }
+        fetchData();
+    }, []);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
     return(
         <form action={create}>
         <div className="flex h-screen">
@@ -43,16 +95,15 @@ export default function Board() {
                             <div className="text-2xl tracking-wide content-center pt-20">Día 1</div>
                             <div className="col-span-11 grid grid-rows-3 grid-flow-col gap-x-20 gap-y-2 mt-6">
                                 <div className="text-2xl tracking-wide text-center">Dominicana</div>
-                                <div className="content-center"><input type="text" id="day1dom1" name="day1dom1" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" id="day1dom2" name="day1dom2" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day1dom1" name="day1dom1" value={formData.day1dom1} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day1dom2" name="day1dom2" value={formData.day1dom2} onChange={handleChange} className="w-64 h-10"></input></div>
                                 <div className="row-start-2 text-2xl tracking-wide content-center">VS</div>
                                 <div className="text-2xl tracking-wide content-center">VS</div>
                                 <div className="text-2xl tracking-wide text-center">Bolivia</div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="row-start-2 content-center"><input type="text"
-                                                                                   className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day1bol1" name="day1bol1" value={formData.day1bol1} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day1bol2" name="day1bol2" value={formData.day1bol2} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="row-start-2 content-center"><input type="text" id="day1result1" name="day1result1" value={formData.day1result1} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day1result2" name="day1result2" value={formData.day1result2} onChange={handleChange} className="w-64 h-10"></input></div>
                             </div>
                         </div>
 
@@ -61,25 +112,25 @@ export default function Board() {
                         <div className="grid grid-cols-12 gap-2">
                             <div className="row-span-4 text-2xl tracking-wide content-center">Día 2</div>
                             <div className="col-span-11 grid grid-rows-2 grid-flow-col gap-x-20 gap-y-1 mt-6">
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2dom1" name="day2dom1" value={formData.day2dom1} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2dom2" name="day2dom2" value={formData.day2dom2} onChange={handleChange} className="w-64 h-10"></input></div>
                                 <div className="row-span-2 text-2xl tracking-wide content-center">VS</div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="row-span-2 content-center"><input type="text"
+                                <div className="content-center"><input type="text" id="day2bol1" name="day2bol1" value={formData.day2bol1} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2bol2" name="day2bol2" value={formData.day2bol2} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="row-span-2 content-center"><input type="text" id="day2result1" name="day2result1" value={formData.day2result1} onChange={handleChange}
                                                                                   className="w-64 h-16"></input></div>
                             </div>
 
 
                             <div className="col-span-11 grid grid-rows-2 grid-flow-col gap-x-20 gap-y-2 mt-4">
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2dom3" name="day2dom3" value={formData.day2dom3} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2dom4" name="day2dom4" value={formData.day2dom4} onChange={handleChange} className="w-64 h-10"></input></div>
                                 <div className="text-2xl tracking-wide content-center">VS</div>
                                 <div className="text-2xl tracking-wide content-center">VS</div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
-                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2bol3" name="day2bol3" value={formData.day2bol3} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2bol4" name="day2bol4" value={formData.day2bol4} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2result2" name="day2result2" value={formData.day2result2} onChange={handleChange} className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" id="day2result3" name="day2result3" value={formData.day2result3} onChange={handleChange} className="w-64 h-10"></input></div>
                             </div>
                         </div>
                     </div>
