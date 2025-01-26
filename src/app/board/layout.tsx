28b2898647c3426124a48bd7cf929a83f7a00613
Jacobo Villarreal <jvillarreal@neural.com.do>
2025-01-25 20:40:07 -0400

    Style changes

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -24,11 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
+    <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    >
+    {children}
+    </body>
     </html>
   );
 }
--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -2,8 +2,8 @@ export default function Board() {
     return(
         <div className="flex h-screen">
 
-            <div className="flex flex-col m-10 flex-grow">
-                <div className="flex flex-row justify-center h-1/5 mb-10">
+            <div className="flex flex-col m-5 flex-grow">
+                <div className="flex flex-row justify-center h-1/5 mb-2">
                     <div className="flex justify-end w-1/3 "><img src="/logos/davis-cup.png" className="object-contain"></img></div>
                     <div className="flex justify-center w-1/3 mx-32 "><img src="/logos/itf.png"></img></div>
                     <div className="flex justify-start w-1/3 "><img src="/logos/fedotenis.png" className="object-contain"></img></div>
@@ -27,43 +27,49 @@ export default function Board() {
 
                     <div className="flex flex-col justify-center flex-grow w-10/12 mx-auto">
 
-                        <div className="grid grid-rows-3 grid-flow-col gap-2 mt-10">
-                            <div className="row-start-2 row-span-2 text-2xl tracking-wide content-center">Día 1</div>
-                            <div className="text-2xl tracking-wide text-center">Dominicana</div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="row-start-2 text-2xl tracking-wide content-center">VS</div>
-                            <div className="text-2xl tracking-wide content-center">VS</div>
-                            <div className="text-2xl tracking-wide text-center">Bolivia</div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="row-start-2 content-center"><input type="text"
-                                                                               className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
+                        <div className="grid grid-cols-12 gap-2 mt-2">
+                            <div className="text-2xl tracking-wide content-center pt-20">Día 1</div>
+                            <div className="col-span-11 grid grid-rows-3 grid-flow-col gap-x-20 gap-y-2 mt-6">
+                                <div className="text-2xl tracking-wide text-center">Dominicana</div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="row-start-2 text-2xl tracking-wide content-center">VS</div>
+                                <div className="text-2xl tracking-wide content-center">VS</div>
+                                <div className="text-2xl tracking-wide text-center">Bolivia</div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="row-start-2 content-center"><input type="text"
+                                                                                   className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                            </div>
                         </div>
 
-                        <div className="grid grid-rows-4 grid-flow-col gap-2 mt-10">
+                        <div className="border-t-4 border-white-300 w-full font-bold mx-auto mt-5"></div>
+
+                        <div className="grid grid-cols-12 gap-2">
                             <div className="row-span-4 text-2xl tracking-wide content-center">Día 2</div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
+                            <div className="col-span-11 grid grid-rows-2 grid-flow-col gap-x-20 gap-y-1 mt-6">
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="row-span-2 text-2xl tracking-wide content-center">VS</div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="row-span-2 content-center"><input type="text"
+                                                                                  className="w-64 h-16"></input></div>
+                            </div>
 
-                            <div className="row-span-2 text-2xl tracking-wide content-center">VS</div>
-                            <div className="text-2xl tracking-wide content-center">VS</div>
-                            <div className="text-2xl tracking-wide content-center">VS</div>
 
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-
-                            <div className="row-span-2 content-center"><input type="text" className="w-72 h-16"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-                            <div className="content-center"><input type="text" className="w-72 h-10"></input></div>
-
+                            <div className="col-span-11 grid grid-rows-2 grid-flow-col gap-x-20 gap-y-2 mt-4">
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="text-2xl tracking-wide content-center">VS</div>
+                                <div className="text-2xl tracking-wide content-center">VS</div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                                <div className="content-center"><input type="text" className="w-64 h-10"></input></div>
+                            </div>
                         </div>
-
                     </div>
                 </main>
 
