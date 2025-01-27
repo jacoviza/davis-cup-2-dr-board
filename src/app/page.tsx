b6142d87771a35b6b6ed0a2c3e9541d10d17eed7
Jacobo Villarreal <jvillarreal@neural.com.do>
2025-01-26 20:44:40 -0400

    Removed unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { neon } from '@neondatabase/serverless';
 import {useRouter} from "next/navigation";
 
 export default async function Page() {
 
-  // redirect to the board page
     const router = useRouter();
     router.push('/board');
 
